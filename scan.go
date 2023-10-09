62456934b068d106353d65d3c07aa360dc1c9bb0
Karol Moroz <karol@moroz.dev>
2023-10-09 19:31:52 +0200

    Move Scan() to separate file

---
+++ scan.go
@@ -0,0 +1,43 @@
+package uuidv7
+
+import "fmt"
+
+func (u *UUID) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case nil:
+		return nil
+
+	case string:
+		// return a null UUID when called with empty string
+		if src == "" {
+			return nil
+		}
+
+		parsed, err := Parse(src)
+		if err != nil {
+			return fmt.Errorf("Scan: %v", err)
+		}
+
+		*u = parsed
+
+	case []byte:
+		// return a null UUID when called with an empty slice
+		if len(src) == 0 {
+			return nil
+		}
+
+		// If the length of the slice is 16 bytes, treat it as
+		// an existing UUID
+		if len(src) == 16 {
+			copy((*u)[:], src)
+			return nil
+		}
+
+		// For any other length, try parsing it as a string
+		return u.Scan(string(src))
+
+	default:
+		return fmt.Errorf("Scan: unable to scan type %T into UUID", src)
+	}
+	return nil
+}
--- uuid.go
+++ uuid.go
@@ -14,45 +14,6 @@ func (u UUID) Value() (driver.Value, error) {
 	return driver.Value(u[:]), nil
 }
 
-func (u *UUID) Scan(src interface{}) error {
-	switch src := src.(type) {
-	case nil:
-		return nil
-
-	case string:
-		// return a null UUID when called with empty string
-		if src == "" {
-			return nil
-		}
-
-		parsed, err := Parse(src)
-		if err != nil {
-			return fmt.Errorf("Scan: %v", err)
-		}
-
-		*u = parsed
-
-	case []byte:
-		// return a null UUID when called with an empty slice
-		if len(src) == 0 {
-			return nil
-		}
-
-		// If the length of the slice is 16 bytes, treat it as
-		// an existing UUID
-		if len(src) == 16 {
-			copy((*u)[:], src)
-			return nil
-		}
-
-		return u.Scan(string(src))
-
-	default:
-		return fmt.Errorf("Scan: unable to scan type %T into UUID", src)
-	}
-	return nil
-}
-
 func Parse(source string) (UUID, error) {
 	var uuid UUID
 	if len(source) != 36 && len(source) != 32 {
